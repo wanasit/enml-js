716cd5841ef908df64def8999566e0549cd7b645
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2012-07-29 20:48:55 +0700

    Browser-Side example +  Refactor

--- example/node-js/example.js
+++ example/node-js/example.js
@@ -19,13 +19,29 @@ console.log(enml0)
 var enml1 = fs.readFileSync('../ex1.enml','utf8');
 var html1 = enml.HTMLOfENML(enml1);
 
-console.log("================ Example 0 (ENML) ================")
+console.log("================ Example 1 (ENML) ================")
 console.log(enml1)
 
-console.log("================ Example 0 (HTML) ================")
+console.log("================ Example 1 (HTML) ================")
 console.log(html1)
 
+var note2 = fs.readFileSync('../note2.json','utf8');
+var shardId = '48' //HARDCODE...
+note2 = JSON.parse(note2);
 
+var resources = {};
+for(var i in note2.resources){
+	var resource = note2.resources[i];
+	resources[resource.data.bodyHash] = enml.URLOfResource(resource.guid, shardId);
+}
+
+var html2 = enml.HTMLOfENML(note2.content, resources);
+
+console.log("================ Example 2 (ENML) ================")
+console.log(note2.content)
+
+console.log("================ Example 2 (HTML) ================")
+console.log(html2)
 
 
 
